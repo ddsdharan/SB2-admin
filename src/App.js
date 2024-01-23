7f7ab7d27e917b5dcb5cadc471e78e370d6d1d78
ddsdharan <ddsdharan@outlook.com>
2024-01-23 17:05:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './components/DashboardPage/Dashboard';
+import ButtonPage from './components/ComponentsPage/ButtonsPage/ButtonPage';
+import Cards from './components/ComponentsPage/CardsPage/Cards';
+import Colors from './components/UtilitiesPage/ColorsPage/Colors';
+import Borders from './components/UtilitiesPage/BordersPage/Borders';
+import Animations from './components/UtilitiesPage/AnimationsPage/Animations';
+import Others from './components/UtilitiesPage/OthersPage/Others';
+import Login from './components/AddonPages/Login';
+import Register from './components/AddonPages/Register';
+import ForgotPassword from './components/AddonPages/ForgotPassword';
+import ErrorPage from './components/AddonPages/ErrorPage';
+import BlankPage from './components/AddonPages/BlankPage';
+import Charts from './components/ChartsPage/Charts';
+import Tables from './components/TablesPage/Tables';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/buttons" element={<ButtonPage />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/colors" element={<Colors />} />
+        <Route path="/borders" element={<Borders />} />
+        <Route path="/animations" element={<Animations />} />
+        <Route path="/others" element={<Others />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/errorpage" element={<ErrorPage />} />
+        <Route path="/blankpage" element={<BlankPage />} />
+        <Route path="/charts" element={<Charts />} />
+        <Route path="/tables" element={<Tables />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
